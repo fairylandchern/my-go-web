e1d3532ced5bc61eaa494f45b0541994c0150f31
fairylandchern <fairylandchern@gmail.com>
2018-04-03 00:22:55 +0800

    update the web projects to api projects

--- web/main.go
+++ web/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	_ "my-go-web/web/routers"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
 	beego.Run()
 }
-
