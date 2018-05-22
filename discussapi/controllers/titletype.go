fcb1b6e64f7ed9ab4cb20fc6ef9352426c0b72e6
fairylandchern <fairylandchern@gmail.com>
2018-05-23 00:27:27 +0800

    fix some error in titletype(log.fatal)

--- discussapi/controllers/titletype.go
+++ discussapi/controllers/titletype.go
@@ -1,15 +1,20 @@
 package controllers
 
-import "log"
+import (
+	"log"
+	"github.com/astaxie/beego/logs"
+)
 
 type TitleTypeController struct {
 	BaseController
 }
 
+
 //@descriptor get the main title type
 //@router /createmaintype [post]
 func (this *TitleTypeController) CreateMainType()  {
-	log.Fatal(string(this.Ctx.Input.RequestBody))
-	this.Data["json"]="hello";
+	log.Println(this.Ctx.Input.RequestBody)	//果然不能用log.fatal，否则会报错，并且程序停止运行,以字节的形式打印输出（数字，所以没有看出来）
+	logs.Error(string(this.Ctx.Input.RequestBody))
+	this.Data["json"]=Response{200,"no error","data is success"};
 	this.ServeJSON()
 }
