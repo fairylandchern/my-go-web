ac0e46b632d7750d8b2c01db6738b96fd1b11c47
fairylandchern <fairylandchern@gmail.com>
2018-06-03 14:54:00 +0800

    change little thing

--- discussapi/controllers/issue_comment.go
+++ discussapi/controllers/issue_comment.go
@@ -1,10 +1,11 @@
 package controllers
 
 import (
-	"github.com/astaxie/beego"
 	"encoding/json"
 	"my-go-web/discussapi/models"
 	"strconv"
+
+	"github.com/astaxie/beego"
 )
 
 type IssueCommentController struct {
@@ -12,77 +13,77 @@ type IssueCommentController struct {
 }
 
 //@router /create [post]
-func (this *IssueCommentController)Create()  {
-	beego.Info("IssueComment::begin",string(this.Ctx.Input.RequestBody))
+func (this *IssueCommentController) Create() {
+	beego.Info("IssueComment::begin", string(this.Ctx.Input.RequestBody))
 	var issue models.IssueComment
-	err:=json.Unmarshal(this.Ctx.Input.RequestBody,&issue)
-	if err!=nil{
-		beego.Info("parsed error",err)
-		this.Data["json"]=Response{INSERTERR,"解析帖子评论失败",nil}
-	}else  if err=issue.Create();err!=nil{
-		this.Data["json"]=Response{INSERTERR,"插入数据出错",nil}
-		beego.Info("err is:",err)
-	}else {
-		this.Data["json"]=Response{SUCCESS,"插入成功",nil}
+	err := json.Unmarshal(this.Ctx.Input.RequestBody, &issue)
+	beego.Info("the comment is", issue.ComentContent)
+	if err != nil {
+		beego.Info("parsed error", err.Error())
+		this.Data["json"] = Response{INSERTERR, "解析帖子评论失败", nil}
+	} else if err = issue.Create(); err != nil {
+		this.Data["json"] = Response{INSERTERR, "插入数据出错", nil}
+		beego.Info("err is:", err.Error())
+	} else {
+		this.Data["json"] = Response{SUCCESS, "插入成功", nil}
 	}
 	this.ServeJSON()
 }
 
 //@router /update [post]
-func (this *IssueCommentController) Update()  {
-	beego.Info("issue::begin",string(this.Ctx.Input.RequestBody))
+func (this *IssueCommentController) Update() {
+	beego.Info("issue::begin", string(this.Ctx.Input.RequestBody))
 	var issue models.IssueComment
-	err:=json.Unmarshal(this.Ctx.Input.RequestBody,&issue)
-	if err!=nil{
+	err := json.Unmarshal(this.Ctx.Input.RequestBody, &issue)
+	if err != nil {
 		beego.Info("parsed error")
-		this.Data["json"]=Response{UPDATEERR,"更新解析失败",nil}
+		this.Data["json"] = Response{UPDATEERR, "更新解析失败", nil}
 	}
-	err=issue.Update()
-	if err!=nil{
+	err = issue.Update()
+	if err != nil {
 		beego.Info("更新失败")
-		this.Data["json"]=Response{UPDATEERR,"更新解析失败",nil}
-	}else {
-		this.Data["json"]=Response{SUCCESS,"更新成功",issue}
+		this.Data["json"] = Response{UPDATEERR, "更新解析失败", nil}
+	} else {
+		this.Data["json"] = Response{SUCCESS, "更新成功", issue}
 	}
 	this.ServeJSON()
 }
 
 //@router /delete [post]
-func (this *IssueCommentController)Delete()  {
-	beego.Info("issue::begin",string(this.Ctx.Input.RequestBody))
+func (this *IssueCommentController) Delete() {
+	beego.Info("issue::begin", string(this.Ctx.Input.RequestBody))
 	var issue models.IssueComment
-	err:=json.Unmarshal(this.Ctx.Input.RequestBody,&issue)
-	if err!=nil{
-		beego.Info("parsed error",err.Error())
-		this.Data["json"]=Response{INSERTERR,"解析帖子失败",nil}
+	err := json.Unmarshal(this.Ctx.Input.RequestBody, &issue)
+	if err != nil {
+		beego.Info("parsed error", err.Error())
+		this.Data["json"] = Response{INSERTERR, "解析帖子失败", nil}
 	}
-	err=issue.Delete()
-	if err!=nil{
+	err = issue.Delete()
+	if err != nil {
 		beego.Info("删除失败")
-		this.Data["json"]=Response{DELETEERR,"删除解析失败",nil}
-	}else {
-		this.Data["json"]=Response{SUCCESS,"删除成功",nil}
+		this.Data["json"] = Response{DELETEERR, "删除解析失败", nil}
+	} else {
+		this.Data["json"] = Response{SUCCESS, "删除成功", nil}
 	}
 	this.ServeJSON()
 }
 
 //@router /querybyissue/:id [get]
-func (this *IssueCommentController) QueryByIssue()  {
-	beego.Info("issue::begin",string(this.Ctx.Input.RequestBody))
-	str_issueid:=this.Ctx.Input.Param(":id")
-	issueid,err:=strconv.ParseInt(str_issueid,10,64)
-	if err!=nil{
-		beego.Info("转换失败",err.Error())
-		this.Data["json"]=Response{READERR,"",nil}
+func (this *IssueCommentController) QueryByIssue() {
+	beego.Info("issue::begin", string(this.Ctx.Input.RequestBody))
+	str_issueid := this.Ctx.Input.Param(":id")
+	issueid, err := strconv.ParseInt(str_issueid, 10, 64)
+	if err != nil {
+		beego.Info("转换失败", err.Error())
+		this.Data["json"] = Response{READERR, "", nil}
 		return
 	}
-	issues,err:=models.QueryCommentByIssueId(issueid)
-	if err!=nil{
-		this.Data["json"]=Response{READERR,"",nil}
+	issues, err := models.QueryCommentByIssueId(issueid)
+	if err != nil {
+		this.Data["json"] = Response{READERR, "", nil}
 		beego.Info("查询失败")
-	}else {
-		this.Data["json"]=Response{SUCCESS,"",issues}
+	} else {
+		this.Data["json"] = Response{SUCCESS, "", issues}
 	}
-	 defer this.ServeJSON()
+	defer this.ServeJSON()
 }
-
--- discussapi/models/issue_comment.go
+++ discussapi/models/issue_comment.go
@@ -2,61 +2,62 @@ package models
 
 import (
 	"time"
+
 	"github.com/astaxie/beego/orm"
 )
 
 type IssueComment struct {
-	CommentId int `json:"comment_id" orm:"column(comment_id);pk;auto;unique"`
-	IssueId int `json:"issue_id" orm:"column(issue_id)"`
-	UserId  int `json:"user_id"  orm:"column(user_id)"`
-	ComentContent string `json:"coment_content" orm:"column(coment_content);size(200)"`
-	Created time.Time `json:"created,omitempty" orm:"column(created);auto_now_add;type(datetime)"`
-	Updated time.Time `json:"updated,omitempty" orm:"column(updated);auto_now_add;type(datetime)"`
+	CommentId     int       `json:"comment_id" orm:"column(comment_id);pk;auto;unique"`
+	IssueId       int       `json:"issue_id" orm:"column(issue_id)"`
+	UserId        int       `json:"user_id"  orm:"column(user_id)"`
+	ComentContent string    `json:"comment_content" orm:"column(coment_content);size(200)"`
+	Created       time.Time `json:"created,omitempty" orm:"column(created);auto_now_add;type(datetime)"`
+	Updated       time.Time `json:"updated,omitempty" orm:"column(updated);auto_now_add;type(datetime)"`
 }
 
 func init() {
 	orm.RegisterModel(new(IssueComment))
 }
 
-func (this *IssueComment)TableName() string {
+func (this *IssueComment) TableName() string {
 	return TableName("issue_comment")
 }
 
-func IssueCommentQueryer() orm.QuerySeter  {
-	return  orm.NewOrm().QueryTable(new(IssueComment))
+func IssueCommentQueryer() orm.QuerySeter {
+	return orm.NewOrm().QueryTable(new(IssueComment))
 }
 
-func (this *IssueComment)Create() error {
-	_,err:= orm.NewOrm().Insert(this)
-	return  err
-}
-
-func (this *IssueComment)Update() error {
-	_,err:=orm.NewOrm().Update(this,"ComentContent")
-	return  err
-}
-
-func (this *IssueComment) Delete() error {
-	_,err:=orm.NewOrm().Delete(this)
+func (this *IssueComment) Create() error {
+	_, err := orm.NewOrm().Insert(this)
 	return err
 }
 
-func QueryCommentByIssueId(issueid int64)([]*IssueComment,error){
+func (this *IssueComment) Update() error {
+	_, err := orm.NewOrm().Update(this, "ComentContent")
+	return err
+}
+
+func (this *IssueComment) Delete() error {
+	_, err := orm.NewOrm().Delete(this)
+	return err
+}
+
+func QueryCommentByIssueId(issueid int64) ([]*IssueComment, error) {
 	var comments []*IssueComment
-	_,err:=IssueCommentQueryer().Filter("IssueId",issueid).All(&comments)
-	if err!=nil{
-		return  nil,err
-	}else {
-		return comments,nil
+	_, err := IssueCommentQueryer().Filter("IssueId", issueid).All(&comments)
+	if err != nil {
+		return nil, err
+	} else {
+		return comments, nil
 	}
 }
 
-func QueryCommentByUserId(userid int) ([]*IssueComment,error)  {
+func QueryCommentByUserId(userid int) ([]*IssueComment, error) {
 	var comments []*IssueComment
-	_,err:=IssueCommentQueryer().Filter("UserId",userid).All(&comments)
-	if err!=nil{
-		return nil,err
-	}else {
-		return comments,nil
+	_, err := IssueCommentQueryer().Filter("UserId", userid).All(&comments)
+	if err != nil {
+		return nil, err
+	} else {
+		return comments, nil
 	}
 }
